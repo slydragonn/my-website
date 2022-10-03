bae510f3838230f929eb8275e5f74d339c0980dc
slydragonn <alogo18m@gmail.com>
2022-10-03 14:13:44 -0500

    chore: change navbar styles

--- components/Hero/Navbar/Navbar.tsx
+++ components/Hero/Navbar/Navbar.tsx
@@ -18,40 +18,39 @@ export const LinkItem = ({ children, href }: LinkItemProps) => {
 const show = keyframes`
  from {
     opacity: 0;
-    transform: translateX(40px);
+    transform: translateY(-30px);
   }
 
   to {
     opacity: 1;
-    transform: translateX(0px);
+    transform: translateY(0px);
   }
 `
 
 const ListItem = styled.li`
   animation: ${show} 1s;
-  transition: transform 0.3s, font-weight 0.2s, color 0.3s, font-size 0.3s;
+  transition: all .3s;
 
   &:hover {
-    transform: translateX(-40px);
-    font-weight: 400;
+    transform: translateY(-5px);
   }
 `
 
 const Navbar = () => {
   return (
     <nav>
-      <ul className="z-20 relative text-white font-thin sm:text-7xl md:text-8xl text-6xl sm:text-right text-center">
+      <ul className="z-20 absolute top-0 left-0 bottom-0 right-0 m-auto flex justify-around items-center rounded w-[320px] h-[70px] text-white font-thin bg-[#181818]/30 backdrop-blur">
         <ListItem>
-          <LinkItem href="/#">ABOUT</LinkItem>
+          <LinkItem href="/#">about</LinkItem>
         </ListItem>
         <ListItem>
-          <LinkItem href="/#">PROJECTS</LinkItem>
+          <LinkItem href="/#">projects</LinkItem>
         </ListItem>
         <ListItem>
-          <LinkItem href="/#">POSTS</LinkItem>
+          <LinkItem href="/#">posts</LinkItem>
         </ListItem>
         <ListItem>
-          <LinkItem href="/#">BLOG</LinkItem>
+          <LinkItem href="/#">blog</LinkItem>
         </ListItem>
       </ul>
     </nav>
