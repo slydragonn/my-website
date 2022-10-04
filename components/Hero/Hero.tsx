10e4087f149e309075202976226c98553fab805e
slydragonn <alogo18m@gmail.com>
2022-10-04 14:59:00 -0500

    fix(hero): hydration - initial ui

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 import { COLORS } from 'utils'
 import Navbar from './Navbar'
 
-const GradientText = styled.p`
+const GradientText = styled.div`
   margin-bottom: 10px;
   border-top: 1px solid ${COLORS.gray};
   border-bottom: 1px solid ${COLORS.gray};
