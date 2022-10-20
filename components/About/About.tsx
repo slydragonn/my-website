b9d89565030d81712f226d9047c9ccee2ff781b1
slydragonn <alogo18m@gmail.com>
2022-10-20 14:15:40 -0500

    chore: add find on me section and refactor business card

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,6 +1,7 @@
 import { LinkButton, Text } from 'components'
 import Image from 'next/image'
 import Skills from './Skills'
+import { SiTwitter as TwitterIcon, SiYoutube as YoutubeIcon } from 'react-icons/si'
 
 const AboutSection = () => {
   return (
@@ -17,7 +18,7 @@ const AboutSection = () => {
         <div className="flex flex-col justify-start md:justify-between pl-0 md:pl-20">
           <div data-aos="zoom-in" className="bg-[#1d1d1d] p-4 rounded shadow-sm text-center mt-4 md:mt-0">
             <span className="text-white font-thin text-base md:text-lg">
-              Hi! I&apos;m Front-End Developer / Designer
+              Hi! I&apos;m a 🚀 Front-End Developer | 💻 Digital Craftsman
             </span>
           </div>
           <span className="lg:w-[450px] w-full mb-2 lg:mt-10 mt-2">
@@ -26,6 +27,27 @@ const AboutSection = () => {
               programming and I love creating new projects.
             </Text>
           </span>
+          <section className='flex items-center my-2 sm:my-0'>
+            <Text>Find me on:</Text>
+            <ul className='flex ml-4'>
+              <li className='mr-3'>
+                <a 
+                className='text-gray-300 font-thin hover:text-[#DE4C36] transition-all duration-300' 
+                href='https://twitter.com/slydragonn' 
+                target='_blank' rel="noreferrer">
+                  <TwitterIcon className='w-[20px] h-[20px]' />
+                </a>
+              </li>
+              <li>
+                <a 
+                className='text-gray-300 font-thin hover:text-[#DE4C36] transition-all duration-300' 
+                href='https://www.youtube.com/channel/UCzTcqvA5oGFZA8RQH3kMlkQ/about' 
+                target='_blank' rel="noreferrer">
+                  <YoutubeIcon className='w-[20px] h-[20px]' />
+                </a>
+              </li>
+            </ul>
+          </section>
           <LinkButton message="On the web" href="/social" isExternal={false} />
         </div>
       </section>
