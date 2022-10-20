dcdaa5bec6eb731077aca56ddebb8cff084d3061
slydragonn <alogo18m@gmail.com>
2022-10-20 12:18:46 -0500

    refactor: move myPosts to utils

---
+++ utils/posts.ts
@@ -0,0 +1,34 @@
+const myPosts = [
+  {
+    id: 'post-5',
+    title: 'Interesting resources for front-end developers',
+    date: '2022.10',
+    url: 'https://slyblog.vercel.app/posts/resources-for-frontend-developers'
+  },
+  {
+    id: 'post-4',
+    title: 'El uso del operador ternario vs if & else',
+    date: '2022.09',
+    url: 'https://www.linkedin.com/pulse/el-uso-del-operador-ternario-vs-else-alejandro-londo%C3%B1o-gomez'
+  },
+  {
+    id: 'post-3',
+    title: 'Mis características preferidas de JavaScript',
+    date: '2022.08',
+    url: 'https://www.linkedin.com/pulse/mis-caracter%C3%ADsticas-preferidas-de-javascript-alejandro-londo%C3%B1o-gomez'
+  },
+  {
+    id: 'post-2',
+    title: '3 recursos imprescindibles para todo desarrollador front-end',
+    date: '2022.08',
+    url: 'https://www.linkedin.com/pulse/3-recursos-imprescindibles-para-todo-desarrollador-londo%C3%B1o-gomez'
+  },
+  {
+    id: 'post-1',
+    title: '¿Qué es el Scope? y su utilidad para un código sostenible.',
+    date: '2022.07',
+    url: 'https://medium.com/@slydragonn/qu%C3%A9-es-el-scope-y-su-utilidad-para-un-c%C3%B3digo-sostenible-5f64dfad57f6'
+  }
+]
+
+export default myPosts
